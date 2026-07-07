84daeae41e2cbf0a41635f91b1a66ccfe87ee7c0
nicosobreira <nicolau.sobreira@gmail.com>
2026-07-07 14:08:26 -0300

    add: insertion sort testing

---
+++ tests/algorithms/test_insertion_sort.c
@@ -0,0 +1,43 @@
+#include "unity.h"
+
+#include "algo/sorting.h"
+#include "ds/list.h"
+
+void setUp(void)
+{
+}
+void tearDown(void)
+{
+}
+
+static void test_single_element(void)
+{
+    int array[] = {1};
+    List list = List_Load(array, 1, sizeof(int));
+
+    insertion_sort(&list, int_le);
+
+    TEST_ASSERT_EQUAL_INT_ARRAY(array, list.ptr, 1);
+}
+
+static void test_general_case(void)
+{
+    int array[] = {3, 2, 1};
+    List list = List_Load(array, 3, sizeof(int));
+
+    insertion_sort(&list, int_le);
+
+    int expected[] = {1, 2, 3};
+
+    TEST_ASSERT_EQUAL_INT_ARRAY(expected, list.ptr, 3);
+}
+
+int main(void)
+{
+    UNITY_BEGIN();
+
+    RUN_TEST(test_single_element);
+    RUN_TEST(test_general_case);
+
+    return UNITY_END();
+}
