552e1d32551249b626a27f41739e946ef46b4cab
nicosobreira <nicolau.sobreira@gmail.com>
2026-07-13 12:05:44 -0300

    add: loop invariant of `insertion_sort`

--- libs/algorithms/src/sort/insertion_sort.c
+++ libs/algorithms/src/sort/insertion_sort.c
@@ -3,10 +3,24 @@
 #include <stdlib.h>
 #include <string.h>
 
-#include "compare/core.h"
-
 #include "ds/list.h"
 
+/**
+Loop invariant.
+
+    Initialization:
+At the start j = 1, so the subarray list[0..j - 1] has one element (list[0]). And so, it's sorted.
+The subarray list[j..n] isn't sorted yet.
+
+    Maintain:
+Let i = j and key = list[j]. The subarray list[0..i - 1] is sorted prior to the while loop.
+If i > 0 and key < list[i - 1], then the key is the smallest value comparing to list[i..j], but we don't know if this is
+true for the subarray list[0..i - 1], so we decrement i by 1.
+If i == 0 or key > list[i], then key is the smallest element in the subarray list[i..j].
+
+    Finalization
+When j = n - 1, the subarray list[0..n - 1] is sorted.
+*/
 void insertion_sort(List *list)
 {
     for (size_t j = 1; j < List_Size(list); ++j)
