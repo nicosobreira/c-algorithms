2033fbf4fa5d24ea6653815813836ef3cb51597f
nicosobreira <nicolau.sobreira@gmail.com>
2026-07-19 15:41:09 -0300

    ref: compare

--- libs/compare/src/core/operations.c
+++ libs/compare/src/core/operations.c
@@ -10,19 +10,19 @@ bool Compare_Less(const Compare *self, const void *a, const void *b)
     return self->cmp(a, b) < 0;
 }
 
+bool Compare_Greater(const Compare *self, const void *a, const void *b)
+{
+    return self->cmp(a, b) > 0;
+}
+
 bool Compare_LessEquals(const Compare *self, const void *a, const void *b)
 {
-    return (bool)(Compare_Less(self, a, b) || Compare_Equals(self, a, b));
+    return (bool)!Compare_Greater(self, a, b);
 }
 
 bool Compare_NotEqual(const Compare *self, const void *a, const void *b)
 {
     return (bool)!Compare_Equals(self, a, b);
-}
-
-bool Compare_Greater(const Compare *self, const void *a, const void *b)
-{
-    return self->cmp(a, b) > 0;
 }
 
 bool Compare_GreaterEquals(const Compare *self, const void *a, const void *b)
